02f89a92620b8a0ece5deff72e624ec93b258bb1
Jatin917 <jkchandel02@gmail.com>
2024-07-04 23:17:22 +0530

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // eslint-disable-next-line no-unused-vars
 const firebaseConfig = {
-  apiKey: 'AIzaSyDLJth4tCdwqbXBJp-ngIb3yRPQqzJj5hA',
-  authDomain: 'netflix-clone-afb70.firebaseapp.com',
-  projectId: 'netflix-clone-afb70',
-  storageBucket: 'netflix-clone-afb70.appspot.com',
-  messagingSenderId: '105579100062',
-  appId: '1:105579100062:web:9540ee7ebb9ee5d0d4488f',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
